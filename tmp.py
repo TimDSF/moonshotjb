6380ebb3ca954c422c8831edc8da9754748c5f7b
Tim Dong <timdong@TimDong.local>
2022-06-20 23:14:49 -0700

    Auto Commit

--- tmp.py
+++
@@ -1,8 +0,0 @@
-import bcrypt
-
-from db import db, st
-from flask import request
-from flask_cors import CORS
-from flask import Flask, jsonify
-
-print(db.child('applicants').child('timdsf').get().val()['hashpw'])
